d853548fe055f1bdb3da057bbb809b5ae355622a
ValLR <valentinalopezrecabal@gmail.com>
2017-07-25 03:33:48 -0400

    ultimos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 	$(".dropdown-button").dropdown();
 	$(".button-collapse").sideNav();
+		$('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrainWidth: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'left', // Displays dropdown with edge aligned to the left of button
+        stopPropagation: false // Stops event propagation
+    });
+
 	$("#c1").on("click", function(e){
 		if((($("#carrousel").attr("class"))=="slide2")){
 			$("#carrousel").removeClass("slide2").addClass("slide");
